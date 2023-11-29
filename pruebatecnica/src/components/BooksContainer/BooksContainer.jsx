46c51f80a9294412ded63bab82620614995fc66e
matias collavini <mncollavini@gmail.com>
2023-11-29 14:33:41 -0300

    Agregue funcionalidad al proyecto

--- pruebatecnica/src/components/BooksContainer/BooksContainer.jsx
+++ pruebatecnica/src/components/BooksContainer/BooksContainer.jsx
@@ -1,39 +1,125 @@
-// import { useEffect, useState } from "react"
-import "./BooksContainer.css"
-import books from "./books.json"
+import React, { useState } from 'react';
+import "./BooksContainer.css";
+import books from "./books.json";
 
 export const BooksContainer = () => {
-    return (
+    const [library, setLibrary] = useState(books.library);
+    const [cart, setCart] = useState([]);
+    const [selectedGenre, setSelectedGenre] = useState('Todos');
+    const [selectedYear, setSelectedYear] = useState('Todos');
 
-        <>
-        <div className="home-container">
-            <h2>Libros ({books.library.length} disponibles)</h2> <br />
-            <h4>Hacé click en el libro que te guste para añadirlo a la lista delectura</h4>
-        </div>
-        <div className="wrapper">
-            <div className="book-container">
-                {
-        books.library.map(books => 
-                                            
-                                                <div className="product" key={books.book.ISBN}> 
-                                            <img className="" src={books.book.cover}/>
-                                            <div className="">
-                                                <p><span>Titulo</span> : {books.book.title}</p>
-                                                <p><span>Genero</span> : {books.book.genre}</p>
-                                                <p><span>Precio</span> : ${books.book.year}</p>
+    const addToCart = (index) => {
+        const selectedBook = library[index];
+        setCart((prevCart) => [...prevCart, selectedBook]);
+        setLibrary((prevLibrary) => prevLibrary.filter((_, i) => i !== index));
+    };
 
-                                            </div>
-                                            </div>
-                                        
-            )
+    const returnToLibrary = (index) => {
+        const returnedBook = cart[index];
+        setCart((prevCart) => prevCart.filter((_, i) => i !== index));
+        setLibrary((prevLibrary) => [...prevLibrary, returnedBook]);
+    };
+
+    const filterLibrary = () => {
+        let filteredLibrary = books.library;
+
+        if (selectedGenre !== 'Todos') {
+            filteredLibrary = filteredLibrary.filter((book) => book.book.genre === selectedGenre);
         }
+
+        if (selectedYear !== 'Todos') {
+            filteredLibrary = filteredLibrary.filter((book) => book.book.year.toString() === selectedYear);
+        }
+
+        setLibrary(filteredLibrary);
+    };
+
+    const resetFilters = () => {
+        setLibrary(books.library);
+        setSelectedGenre('Todos');
+        setSelectedYear('Todos');
+    };
+
+    return (
+        <>
+            <div className="home-container">
+                <h2>Libros ({library.length} disponibles)</h2> <br />
+                <div>
+                    <label htmlFor="genreFilter">Filtrar por género:</label>
+                    <select
+                        id="genreFilter"
+                        onChange={(e) => setSelectedGenre(e.target.value)}
+                        value={selectedGenre}
+                    >
+                        <option value="Todos">Todos</option>
+                        <option value="Fantasía">Fantasía</option>
+                        {/* Otras opciones de género */}
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="yearFilter">Filtrar por año:</label>
+                    <select
+                        id="yearFilter"
+                        onChange={(e) => setSelectedYear(e.target.value)}
+                        value={selectedYear}
+                    >
+                        <option value="Todos">Todos</option>
+                        {/* Otras opciones de año */}
+                    </select>
+                </div>
+                <button onClick={filterLibrary}>Aplicar Filtro</button>
+                <button onClick={resetFilters}>Resetear Filtros</button>
+                <h4>
+                    Haz clic en el libro que te guste para añadirlo a la lista de lectura
+                </h4>
             </div>
-            <div className="cart-container">
-                <div className="cart">
-                    <h2>Lista de lectura</h2>
-                    <h3>Aún no tenés ningun libro en tu lista de lectura...</h3>
+            <div className="wrapper">
+                <div className="book-container">
+                    {library.map((book, index) => (
+                        <div className="product" key={book.book.ISBN} onClick={() => addToCart(index)}>
+                            <img src={book.book.cover} alt={book.book.title} />
+                            <div>
+                                <p>
+                                    <span>Titulo</span> : {book.book.title}
+                                </p>
+                                <p>
+                                    <span>Genero</span> : {book.book.genre}
+                                </p>
+                                <p>
+                                    <span>Precio</span> : ${book.book.year}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className="cart-container">
+                    <div className="cart">
+                        <h2>Lista de lectura ({cart.length})</h2>
+                    </div>
+                    <div className="cart-items-container">
+                        {cart.length === 0 ? (
+                            <h3>Aún no tienes ningún libro en tu lista de lectura...</h3>
+                        ) : (
+                            cart.map((book, index) => (
+                                <div className="product" key={book.book.ISBN} onClick={() => returnToLibrary(index)}>
+                                    <img src={book.book.cover} alt={book.book.title} />
+                                    <div>
+                                        <p>
+                                            <span>Titulo</span> : {book.book.title}
+                                        </p>
+                                        <p>
+                                            <span>Genero</span> : {book.book.genre}
+                                        </p>
+                                        <p>
+                                            <span>Precio</span> : ${book.book.year}
+                                        </p>
+                                    </div>
+                                </div>
+                            ))
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
-)}
+        </>
+    );
+};
